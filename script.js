3385d98edd01b0f8e19be8c465f87a2107bdd4bb
Isaacc <feluopi@gmail.com>
2025-10-20 16:03:29 +0100

    ses js3

--- script.js
+++ script.js
@@ -47,3 +47,91 @@ let select = (e) => document.querySelector(e);
 						this.invalidate().delay(1).restart(true);
 					},
 				});
+                gsap.to('#sec', 0.5, {
+					rotation: getSec,
+					transformOrigin: '50% 50%',
+					ease: 'bounce',
+					onComplete: function () {
+						setTime();
+						if (gsap.getProperty('#sec', 'rotation') >= 360) {
+							gsap.set('#sec', { rotation: 0, transformOrigin: '50% 50%' });
+						}
+						this.invalidate().delay(0).restart(true);
+					},
+				});
+
+				gsap
+					.timeline({
+						repeat: -1,
+						repeatDelay: 5,
+						defaults: { duration: 0.25, ease: 'power1.out' },
+					})
+					.to('#face', { morphSVG: '#face02', repeat: 2, yoyo: true });
+
+				gsap
+					.timeline({
+						repeat: -1,
+						repeatDelay: 5,
+						defaults: { duration: 1, ease: 'bounce' },
+					})
+					.delay(8)
+					.to('#hand-sec', { morphSVG: '#handSec02' })
+					.to('#hand-sec', { morphSVG: '#handSec01' });
+
+				gsap
+					.timeline({
+						repeat: -1,
+						repeatDelay: 18,
+						defaults: { duration: 1, ease: 'bounce' },
+					})
+					.delay(5)
+					.to('#hand-min', { morphSVG: '#handMin02' })
+					.to('#hand-min', { morphSVG: '#handMin01' });
+
+				gsap
+					.timeline({
+						repeat: -1,
+						repeatDelay: 30,
+						defaults: { duration: 1, ease: 'bounce' },
+					})
+					.delay(10)
+					.to('#hand-hr', { morphSVG: '#handHr02' })
+					.to('#hand-hr', { morphSVG: '#handHr01' });
+
+				function setTime(init) {
+					let newDateTime = new Date();
+
+					if (init == true) {
+						gsap.set('#sec', { rotation: getSec, transformOrigin: '50% 50%' });
+					}
+
+					gsap.set('#min', {
+						rotation:
+							newDateTime.getMinutes() * 6 + (new Date().getSeconds() * 6) / 59,
+						transformOrigin: '50% 50%',
+					});
+					gsap.set('#hr', {
+						rotation:
+							newDateTime.getHours() * 30 + (new Date().getMinutes() * 30) / 59,
+						transformOrigin: '50% 50%',
+					});
+				}
+
+				function getSec() {
+					let newDateTime = new Date();
+					let sec = newDateTime.getSeconds();
+					let rotation = sec * 6;
+					if (sec == 0) rotation = 360;
+
+					let difference = Math.abs(
+						gsap.getProperty('#sec', 'rotation') - rotation
+					);
+					if (difference >= 12)
+						gsap.set('#sec', {
+							rotation: rotation,
+							transformOrigin: '50% 50%',
+						});
+
+					return rotation;
+				}
+			}
